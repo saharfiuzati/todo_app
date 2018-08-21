b6611cc9125969449c311a0fbcb1481e9af27b78
sahar fiuzati hagh <saharfh@gmail.com>
2018-08-21 21:24:24 +0300

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,40 @@
+class TodosController < ApplicationController
+	def index
+			  @todos = Todo.all
+	end
+
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		@todo = Todo.create(todo_params)
+		redirect_to todos_path
+	end
+
+	def destroy
+		@todo = Todo.find(params[:id])
+		@todo.destroy
+		redirect_to todos_path
+		
+	end
+
+	def edit
+		@todo = Todo.find(params[:id])
+	end	
+
+	def update
+		@todo = Todo.find(params[:id])
+		if @todo.update(params[:todo].permit(:item))
+			redirect_to todos_path
+		else
+			render 'edit'
+		end
+	end	
+
+			private
+	def todo_params
+		params.require(:todo).permit(:item)
+	end
+	
+end
