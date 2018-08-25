a7fa6ed1a88a9b896bcf9728d76831d9810f19a6
sahar <saharfh@gmail.com>
2018-08-25 20:41:40 +0300

    second try

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todos
+  get 'todos/:id/detail' , to:'todos#show', as: :detail
 end
+
